51d231baed150fb0c02f845aa256b930845758b2
netlopa87@gmail.com <netlopa87@gmail.com@3f9a4a3a-94da-a736-7427-ced22502475b>
2011-10-26 00:23:10 +0000

    TODO: FIX multipleoptions in Settings

--- adm-quiz-game/QuizGame/src/com/lrepafi/quizgame/SettingsActivity.java
+++ adm-quiz-game/QuizGame/src/com/lrepafi/quizgame/SettingsActivity.java
@@ -1,15 +1,19 @@
 package com.lrepafi.quizgame;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.TextView;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.*;
 import com.lrepafi.quizgame.controllers.*;
+import com.lrepafi.quizgame.entities.Settings.Category;
+
 import android.view.View.OnClickListener;
 
 public class SettingsActivity extends Activity {
@@ -34,10 +38,60 @@ public class SettingsActivity extends Activity {
 	        btnPrefs.setOnClickListener(new OnClickListener() {
 				public void onClick(View v) {
 					
+					final CharSequence[] items = {Category.LITERATURE.toString(), "Green", "Blue"};
+					final boolean values[] = {false, false, false};
+
+					AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+					builder.setTitle("Pick a color");
+					/*builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+					    public void onClick(DialogInterface dialog, int item) {
+					        Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+					    }
+					});*/
 					
+					builder.setMultiChoiceItems(items, values, new DialogInterface.OnMultiChoiceClickListener() {
+						
+						public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+							// TODO Auto-generated method stub
+							Toast.makeText(getApplicationContext(), items[which], Toast.LENGTH_SHORT).show();
+							   
+						}
+					});
+					AlertDialog alert = builder.create();
+					alert.show();
+
+					   
 				}
 	        });
+	        
+	        //registerForContextMenu(btnPrefs);
+
 	   }
+	   
+	   /*@Override
+	   public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+
+	   super.onCreateContextMenu(menu, v, menuInfo);
+	   MenuInflater inflater = getMenuInflater();
+	   inflater.inflate(R.menu.prefs_question, menu);
+
+	   }
+	   
+	   @Override 
+	   public boolean onContextItemSelected(MenuItem item) {
+
+	   switch(item.getItemId()) {
+
+	   case R.id.literature :
+	   	//TODO
+	   	return true;
+	   case R.id.science :
+	   	//TODO
+	   	return true;
+	   default:
+	   	return super.onContextItemSelected(item);
+	   }
+	   }*/
 	   
        public class MyOnItemSelectedListener implements OnItemSelectedListener {
 
