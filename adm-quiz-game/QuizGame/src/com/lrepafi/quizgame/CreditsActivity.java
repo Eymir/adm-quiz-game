32dbf90102ae89f304ca7df4ff16aa3b971c5d27
francois.rixain <francois.rixain@3f9a4a3a-94da-a736-7427-ced22502475b>
2011-10-26 14:39:04 +0000

    oooo

--- adm-quiz-game/QuizGame/src/com/lrepafi/quizgame/CreditsActivity.java
+++ adm-quiz-game/QuizGame/src/com/lrepafi/quizgame/CreditsActivity.java
@@ -2,6 +2,10 @@ package com.lrepafi.quizgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class CreditsActivity extends Activity {
@@ -10,7 +14,16 @@ public class CreditsActivity extends Activity {
 	        super.onCreate(savedInstanceState);
 	        
 	        //FIXME Relink the proper layout instead of R.layout.mainmenu once i have .xml layout file
-	        setContentView(R.layout.mainmenu);
+	        setContentView(R.layout.credits);
+	        
+	        final TextView txt1 = (TextView) findViewById(R.id.textView1);
+	        final TextView txt2 = (TextView) findViewById(R.id.textView2);
+	        final TextView txt3 = (TextView) findViewById(R.id.textView3);
+	        Animation anim = AnimationUtils.loadAnimation(this, R.anim.anim_credits); 
+	        txt1.setAnimation(anim);
+	        txt2.setAnimation(anim);
+	        txt3.setAnimation(anim);
+
 	   }
 	   
 }
